e9182bb6f0df5e99c06643d2b11d252d4b09d8de
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-09-29 21:27:00 +0200

    el ejercicio 7_11 hace que crashee el proyecto no se porque

--- Assets/Scripts/Ejercicio7_11.cs
+++ Assets/Scripts/Ejercicio7_11.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ejercicio7_11 : MonoBehaviour
 {
+   [SerializeField] int numero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (numero <= 0 || numero > 100)
+        {
+            Debug.LogWarning("Por favor, introduce un número positivo entre 1 y 100.");
+            return; // Salimos de la función si el número no es válido
+        }
+        Multiplicar();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+   void Multiplicar()
+    {
+        for (int numeroTabla = 1; numeroTabla <= 10; numero++)
+        {
+            int total = numero * numeroTabla;
+            Debug.Log(numero + " x " + numeroTabla + " = " + total);
+        }
     }
 }
