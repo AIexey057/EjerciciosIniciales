a2f703a219ff8e88132cc9177e57d69ed9a8ffff
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-09-29 18:50:08 +0200

    5.5

--- Assets/Scripts/Ejercicio5_5.cs
+++ Assets/Scripts/Ejercicio5_5.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ejercicio5_5 : MonoBehaviour
 {
+    [SerializeField] int nivel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NivelPersonaje();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void NivelPersonaje()
+    {
+        if(nivel % 2 == 0)
+        {
+            Debug.Log("Este número es par");
+        }
+        else
+        {
+            Debug.Log("Este número es impar");
+        }
     }
 }
